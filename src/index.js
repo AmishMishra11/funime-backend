c05f05f5113765df3bcddba4dfeb21c78ae6fc2d
AmishMishra11 <amishmishra11@gmail.com>
2022-12-25 13:03:05 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.port || 9000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express App!");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Users will be listed here...");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
